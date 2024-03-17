2eec7e1c0c11e980e89ae22b0f891eeb087f3dea
Andre Dault <andre@dault.ca>
2024-03-17 14:26:01 -0400

    * y24:w12:d00

--- root/update_cert.sh
+++ root/update_cert.sh
@@ -14,9 +14,10 @@ TODAY=$(date +%Y-%m-%d)
 
 log_message "Starting certificate check script"
 if [ "$MOD_DATE" == "$TODAY" ]; then
-    log_message "The cert was modified today."
+    log_message "*****  The cert was modified today."
     scp /etc/letsencrypt/live/dault.ca/privkey.pem root@192.168.1.1:/data/unifi-core/config/unifi-core.key.test
     scp /etc/letsencrypt/live/dault.ca/fullchain.pem root@192.168.1.1:/data/unifi-core/config/unifi-core.crt.test
+    ssh root@192.168.1.1 chmod a+r /data/unifi-core/config/unifi-core.crt.test
     ssh root@192.168.1.1 systemctl reload unifi-core
 else
     log_message "The cert was not modified today."
